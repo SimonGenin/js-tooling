740665f0c10cfef7779f09548bb1d3dc73c1ca5b
Simon Genin (ges) <ges@odoo.com>
2022-06-09 20:42:56 +0200

    wip

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,5 @@
 declare const owl: typeof import("@odoo/owl/dist/types");
 declare const luxon: typeof import("luxon");
 declare const Qunit: typeof import("qunit");
+// @ts-ignore
+declare const $: typeof import("jquery"); 
